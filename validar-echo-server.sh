41c64d5c39940ff597f956593a42e1b6016f0359
MateoLardiez <mlardiez@fi.uba.ar>
2025-03-21 17:34:28 -0300

    a

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -6,10 +6,7 @@
 
 msg="Mensaje de prueba"
 
-SERVER="server:12345"
-
-
-respuesta=$(echo "$msg" | docker run --rm --network=tp0_testing_net busybox nc server 12345)
+respuesta=$(echo "$msg" | nc server 12345)
 
 if [ "$respuesta" == "$mensaje" ]; then
   echo "action: test_echo_server | result: success"
