31167b9ee0d0e6255aca659c579a72fff682cf98
MateoLardiez <mlardiez@fi.uba.ar>
2025-03-23 15:50:46 -0300

    Agregando solucion final de ej3

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-# docker compose -f docker-compose-levantar-servidor.yaml up -d --build # Levanto red y servidor
-
-# sleep 1
-
 MSG="Mensaje de prueba"
 
-#RESPONSE=$(echo "$MSG" | docker run --rm --network=tp0_testing_net -i busybox nc -w 5 server 12345)
 RESPONSE=$(docker run --rm --network=tp0_testing_net busybox sh -c "echo '$MSG' | nc -w 5 server 12345")
 
-if [ "$RESPONSE" == "$MSG" ]; then
+if [ "$RESPONSE" = "$MSG" ]; then
   echo "action: test_echo_server | result: success"
 else
   echo "action: test_echo_server | result: fail"
 fi
-
-#make docker-compose-down
-
